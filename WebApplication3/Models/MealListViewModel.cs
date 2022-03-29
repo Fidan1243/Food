406df2f9eedfca70e7309f745eb496628f2bc271
Iman_xz90 <Iman_xz90@itstep.edu.az>
2022-03-29 12:46:34 +0400

    Add project files.

---
+++ WebApplication3/Models/MealListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Entities;
+
+namespace WebApplication3.Models
+{
+    public class MealListViewModel
+    {
+        public List<HotMeal> HotMeals { get; set; }
+        public List<Drink> Drinks { get; set; }
+        public List<FastFood> FastFoods { get; set; }
+    }
+}
